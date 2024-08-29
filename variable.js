bf0aaae9b2fd1daf6fd291cd6564dff1c1fdab46
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-08-29 12:05:37 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,28 @@
+// Javascript Variables//
+// node variable.js
+
+
+var aple = 205;
+var tometo = parseFloat("10.5");
+// console.log(aple + tometo);
+
+
+var price = 1.11;
+var amount = 0.3;
+var all = price + amount;
+// console.log(all.toFixed(5));
+
+
+// week bazar 
+var cal = 72*7-14;
+var dal = 130*2/1.3;
+var cini = 145*3;
+var poteto = 60*5-20;
+var piach = 120*10/2;
+var tel = 985-63;
+var pepsi = 145/2;
+var taka = cal+dal+cini+poteto+piach+tel+pepsi
+console.log(taka);
+
+
+
