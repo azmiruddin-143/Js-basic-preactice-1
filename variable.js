47a8a096185ce2779895cb2e6b741a55ea846567
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-08-29 14:48:27 +0600

    All Task Complete

--- variable.js
+++ variable.js
@@ -14,15 +14,56 @@ var all = price + amount;
 
 
 // week bazar 
-var cal = 72*7-14;
-var dal = 130*2/1.3;
-var cini = 145*3;
-var poteto = 60*5-20;
-var piach = 120*10/2;
-var tel = 985-63;
-var pepsi = 145/2;
-var taka = cal+dal+cini+poteto+piach+tel+pepsi
-console.log(taka);
+var cal = 72 * 7 - 14;
+var dal = 130 * 2 / 1.3;
+var cini = 145 * 3;
+var poteto = 60 * 5 - 20;
+var piach = 120 * 10 / 2;
+var tel = 985 - 63;
+var pepsi = 145 / 2;
+var taka = cal + dal + cini + poteto + piach + tel + pepsi
+// console.log(taka);
+
+
+// Task Number-1//
+var mymoney = 1500;
+var oranges = 280 * 3;
+var apples = 220 * 2;
+var cost = oranges + apples
+var retunTaka = mymoney - cost
+// console.log(retunTaka);
+// Ans: 220 taka //
+
+
+// Task Number-2//
+var Mathematics = 10.1;
+var Biology = 75.9;
+var Chemistry = 31.1;
+var Physics = 80;
+var Bangla = 66.2;
+var result = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+// console.log(result);
+
+// All result 263.3clear
+
+
+// Task Number-3//
+
+var amount = 336;
+var remainder = 336 / 5;
+// console.log(remainder);
+
+// Ans 67.2//
 
 
 
+
+// Task Number-3//
+
+var a = isNaN('2-10');
+var s = isNaN(2 - 10);
+console.log(a);
+console.log(s);
+
+// Ans True, false//
